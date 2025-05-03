f621b70bc1a83816a4d5b859ddcd67ae0e852cf9
6Morpheus6 <wanton-decay@gmx.net>
2025-05-03 21:57:22 +0200

    build true

--- start.js
+++ start.js
@@ -2,30 +2,34 @@ module.exports = async (kernel) => {
   return {
     daemon: true,
     run: [{
-      "method": "shell.run",
-      "params": {
-        "path": "app",
-        "conda": "env",
-        "env": {
+      method: "shell.run",
+      params: {
+        build: true,
+        path: "app",
+        conda: "env",
+        env: {
           "DISTUTILS_USE_SDK": "1"
         },
-        "message": [
+        message: [
           "{{platform === 'win32' ? 'set' : 'env'}}",
           "nvcc --version",
           "python app.py",
         ],
-        "on": [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
+        on: [{
+          event: "/http:\/\/[0-9.:]+/",
+          done: true
+        }]
       }
     }, {
-      "method": "local.set",
-      "params": {
-        "url": "{{input.event[0]}}"
+      method: "local.set",
+      params: {
+        url: "{{input.event[0]}}"
       }
     }, {
-      "method": "proxy.start",
-      "params": {
-        "uri": "{{local.url}}",
-        "name": "Local Sharing"
+      method: "proxy.start",
+      params: {
+        uri: "{{local.url}}",
+        name: "Local Sharing"
       }
     }]
   }
